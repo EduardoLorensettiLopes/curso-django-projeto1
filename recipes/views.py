cccf818c21bcca2a0c27f5c1f982ff9157e16b72
Eduardo Lorensetti Lopes <eduardolorensetti@gmail.com>
2023-06-07 20:42:22 -0300

    upgrade

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,14 @@
 # flake8: noqa
 from django.http import HttpResponse
 from django.shortcuts import render
+
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', status=201, context={
+        'name': 'Eduardo',
+    })
+
 def contact(request):
     return HttpResponse("Contact")
+
 def about(request):
     return HttpResponse("About")
