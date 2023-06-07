957ffc049f5122108efa81904b7411ee24f995ae
Eduardo Lorensetti Lopes <eduardolorensetti@gmail.com>
2023-06-07 19:59:04 -0300

    update

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Home")
+    return render(request, 'home.html')
 
 def contact(request):
     return HttpResponse("Contact")
