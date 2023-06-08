695dbbd2adbae9084a284f40af48d758ae368c35
Eduardo Lorensetti Lopes <eduardolorensetti@gmail.com>
2023-06-07 21:03:26 -0300

    Removido Contact and about

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
 # flake8: noqa
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html', status=201, context={
         'name': 'Eduardo',
     })
-
-def contact(request):
-    return HttpResponse("Contact")
-
-def about(request):
-    return HttpResponse("About")
